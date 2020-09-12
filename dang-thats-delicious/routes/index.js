def8096ba09c7fd7fa4507ced44c2813d97cc37b
Carlos Diaz <cdiaz86@gmail.com>
2020-09-12 13:34:57 -0400

    Adds reverse name route.

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const carlos = {
+    name: 'Carlos',
+    age: 100,
+    cool: true,
+  };
+  // res.send('Hey! It works!');
+  // res.json(carlos);
+  // res.send(req.query.name);
+  res.json(req.query)
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
