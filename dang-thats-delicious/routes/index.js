1e710099eaca3c349aa82b5957d62c89d9a8b6f0
Carlos Diaz <cdiaz86@gmail.com>
2020-09-14 20:30:46 -0400

    Wraps controller in higher order error catching function.

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // we identify the URL with the first argument, 2nd serves view.
 router.get('/', storeController.homePage);
 
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
-
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
